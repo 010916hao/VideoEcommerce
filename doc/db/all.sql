90acbbb16d1c740438e8c3ed4f682a87b77facdf
010916hao <hitlyh@126.com>
2020-06-11 20:53:45 -0700

    add 'doc' directory to manage sql

---
+++ doc/db/all.sql
@@ -0,0 +1,9 @@
+drop table if exists `test`;
+
+CREATE TABLE `test` (
+  `id` char(8) DEFAULT '' NOT NULL,
+  `name` varchar(255) DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 comment='test';
+
+insert into `test` (`id`, `name`) values(1, 'yihao');
