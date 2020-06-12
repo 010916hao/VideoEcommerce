1e7d8701b6766daa019fd199663b7d0c704724fb
010916hao <hitlyh@126.com>
2020-06-11 23:06:58 -0700

    1. Integrated Mybatis-generator step 1. add idea plugin step 2. add server/src/main/resources/generator/generatorConfig file step 3. create maven start command 'mybatis-generator:generate -e'

--- system/src/main/java/com/mooc/SystemApplication.java
+++ system/src/main/java/com/mooc/SystemApplication.java
@@ -1,4 +1,4 @@
-package com.mooc.system.config;
+package com.mooc;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
@@ -6,14 +6,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication//can scan all packages in current directory
 @EnableEurekaClient
 @MapperScan("com.mooc.server.mapper")
-@ComponentScan("com.mooc")
+
 public class SystemApplication {
 
 //	public static void main(String[] args) {
