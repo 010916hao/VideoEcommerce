fd99722f3a54c4899b379554162ecf79d74869d6
010916hao <hitlyh@126.com>
2020-06-11 21:33:58 -0700

    1. commit new common module 'server'

---
+++ server/src/main/java/com/mooc/server/mapper/TestMapper.java
@@ -0,0 +1,11 @@
+package com.mooc.server.mapper;
+
+import com.mooc.server.domain.Test;
+
+import java.util.List;
+
+
+public interface TestMapper {
+
+    List<Test> list();
+}
---
+++ server/src/main/java/com/mooc/server/service/TestService.java
@@ -0,0 +1,19 @@
+package com.mooc.server.service;
+
+import com.mooc.server.domain.Test;
+import com.mooc.server.mapper.TestMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class TestService {
+
+    @Resource
+    private TestMapper testMapper;
+
+    public List<Test> list() {
+        return testMapper.list();
+    }
+}
