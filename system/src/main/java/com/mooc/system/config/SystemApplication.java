bf1639cde94d01799f8f51cbba3c54950076ecb5
010916hao <hitlyh@126.com>
2020-06-17 18:33:15 -0400

    1. add vue router 2. change SystemApplication.java to com.mooc.system.config directory

--- system/src/main/java/com/mooc/system/config/SystemApplication.java
+++ system/src/main/java/com/mooc/system/config/SystemApplication.java
@@ -1,4 +1,4 @@
-package com.mooc;
+package com.mooc.system.config;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
@@ -6,12 +6,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication//can scan all packages in current directory
 @EnableEurekaClient
 @MapperScan("com.mooc.server.mapper")
-
+@ComponentScan("com.mooc")
 public class SystemApplication {
 
 //	public static void main(String[] args) {
