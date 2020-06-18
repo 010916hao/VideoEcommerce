c99ee975745120f61f2628984429f26f5b9a2ed2
010916hao <hitlyh@126.com>
2020-06-17 20:00:09 -0400

    1. add welcome children component and welcome children router, you need use children router with router-view ps: children path doesn't need "/" slash.

--- admin/src/router.js
+++ admin/src/router.js
@@ -2,7 +2,7 @@ import Vue from "vue"
 import Router from "vue-router"
 import Login from "./views/login.vue"
 import Admin from "./views/admin.vue"
-// import Welcome from "./views/admin/welcome.vue"
+import Welcome from "./views/admin/welcome.vue"
 // import Category from "./views/admin/category.vue"
 // import Course from "./views/admin/course.vue"
 // import Chapter from "./views/admin/chapter.vue"
@@ -34,61 +34,10 @@ export default new Router({
     path: "/admin",
     name: "admin",
     component: Admin,
-        // meta: {
-        //     loginRequire: true
-        // },
-    //     children: [{
-    //         path: "welcome",
-    //         name: "welcome",
-    //         component: Welcome,
-    //     }, {
-    //         path: "business/category",
-    //         name: "business/category",
-    //         component: Category,
-    //     }, {
-    //         path: "business/course",
-    //         name: "business/course",
-    //         component: Course,
-    //     }, {
-    //         path: "business/chapter",
-    //         name: "business/chapter",
-    //         component: Chapter,
-    //     }, {
-    //         path: "business/section",
-    //         name: "business/section",
-    //         component: Section,
-    //     }, {
-    //         path: "business/content",
-    //         name: "business/content",
-    //         component: Content,
-    //     }, {
-    //         path: "business/teacher",
-    //         name: "business/teacher",
-    //         component: Teacher,
-    //     }, {
-    //         path: "business/member",
-    //         name: "business/member",
-    //         component: Member,
-    //     }, {
-    //         path: "business/sms",
-    //         name: "business/sms",
-    //         component: Sms,
-    //     }, {
-    //         path: "file/file",
-    //         name: "file/file",
-    //         component: File,
-    //     }, {
-    //         path: "system/user",
-    //         name: "system/user",
-    //         component: User,
-    //     }, {
-    //         path: "system/resource",
-    //         name: "system/resource",
-    //         component: Resource,
-    //     }, {
-    //         path: "system/role",
-    //         name: "system/role",
-    //         component: Role,
-    //     }]
+    children: [{
+      path: 'welcome',
+      component: Welcome
+    }]
+
   }]
 })
