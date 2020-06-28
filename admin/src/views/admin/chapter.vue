5643c88c226f556aadbb699c69e297744ebca910
010916hao <hitlyh@126.com>
2020-06-28 17:40:32 -0400

    1. Integrate axios for front-backend interaction 2. add corsconfig class to solve cross-origin resource sharing

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -979,8 +979,16 @@
     name: "chapter",
     mounted: function() {
       // this.$parent.activeSidebar("business-chapter-sidebar");
+      let _this = this;
+      _this.list();
     },
     methods: {
+      list() {
+        let _this = this;
+        _this.$ajax.get('http://127.0.0.1:9002/business/admin/chapter/list').then((response)=>{
+          console.log("Results of quering chapters：", response);
+        })
+      }
     }
   }
 </script>
