1726c582bab404cc91a3d6cc06526ec53c48ab2c
010916hao <hitlyh@126.com>
2020-06-28 18:03:02 -0400

    1. use gateway for api routing and vue pages only send request to gateway

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -97,7 +97,7 @@
     methods: {
       list() {
         let _this = this;
-        _this.$ajax.get('http://127.0.0.1:9002/business/admin/chapter/list').then((response)=>{
+        _this.$ajax.get('http://127.0.0.1:9000/business/admin/chapter/list').then((response)=>{
           console.log("Results of quering chapters：", response);
           _this.chapters = response.data;
         })
