900314eb50d6216f5f7570cfcf0e67f9383a0cda
010916hao <hitlyh@126.com>
2020-07-16 18:19:37 -0400

    1. design section table 2. generate section code

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -18,23 +18,24 @@ create table `chapter` (
     primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment='chapter';
 
-insert into `chapter` (`id`, `course_id`, `name`) values('00000001', '00000000', 'testchapter01');
-insert into `chapter` (`id`, `course_id`, `name`) values('00000002', '00000000', 'testchapter02');
-insert into `chapter` (`id`, `course_id`, `name`) values('00000003', '00000000', 'testchapter03');
-insert into `chapter` (`id`, `course_id`, `name`) values('00000004', '00000000', 'testchapter04');
-insert into `chapter` (`id`, `course_id`, `name`) values('00000005', '00000000', 'testchapter05');
-insert into `chapter` (`id`, `course_id`, `name`) values('00000006', '00000000', 'testchapter06');
-insert into `chapter` (`id`, `course_id`, `name`) values('00000007', '00000000', 'testchapter07');
-insert into `chapter` (`id`, `course_id`, `name`) values('00000008', '00000000', 'testchapter08');
-insert into `chapter` (`id`, `course_id`, `name`) values('00000009', '00000000', 'testchapter09');
-insert into `chapter` (`id`, `course_id`, `name`) values('00000010', '00000000', 'testchapter10');
-insert into `chapter` (`id`, `course_id`, `name`) values('00000011', '00000000', 'testchapter11');
-insert into `chapter` (`id`, `course_id`, `name`) values('00000012', '00000000', 'testchapter12');
-insert into `chapter` (`id`, `course_id`, `name`) values('00000013', '00000000', 'testchapter13');
-insert into `chapter` (`id`, `course_id`, `name`) values('00000014', '00000000', 'testchapter14');
-insert into `chapter` (`id`, `course_id`, `name`) values('00000015', '00000000', 'testchapter15');
-insert into `chapter` (`id`, `course_id`, `name`) values('00000016', '00000000', 'testchapter16');
-insert into `chapter` (`id`, `course_id`, `name`) values('00000017', '00000000', 'testchapter17');
-insert into `chapter` (`id`, `course_id`, `name`) values('00000018', '00000000', 'testchapter18');
-insert into `chapter` (`id`, `course_id`, `name`) values('00000019', '00000000', 'testchapter19');
-insert into `chapter` (`id`, `course_id`, `name`) values('00000020', '00000000', 'testchapter20');
+# insert into `chapter` (`id`, `course_id`, `name`) values('00000001', '00000000', 'testchapter01');
+-- 小节
+drop table if exists `section`;
+create table `section` (
+           `id` char(8) not null default '' comment 'id',
+           `title` varchar(50) not null comment 'Title',
+           `course_id` char(8) comment 'Course|course.id',
+           `chapter_id` char(8) comment 'Chapter|chapter.id',
+           `video` varchar(200) comment 'Video',
+           `time` int comment 'time|unit second',
+           `charge` char(1) comment 'charge|C charge；F free',
+           `sort` int comment 'sort',
+           `created_at` datetime(3) comment 'create time',
+           `updated_at` datetime(3) comment 'modify time',
+           primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='section';
+
+# alter table `section` add column (`vod` char(32) comment 'vod|google vod');
+
+insert into `section` (id, title, course_id, chapter_id, video, time, charge, sort, created_at, updated_at)
+values ('00000001', 'test section 01', '00000001', '00000000', '', 500, 'f', 1, now(), now());
