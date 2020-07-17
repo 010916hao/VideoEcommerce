368c91f710348445475a19e1c4fb555f5dc15c74
010916hao <hitlyh@126.com>
2020-07-17 14:21:02 -0400

    1. Integrate code generator with mybatis generator by dom4j

--- generator/src/main/java/com/mooc/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/mooc/generator/server/ServerGenerator.java
@@ -5,24 +5,52 @@ import com.mooc.generator.util.Field;
 import com.mooc.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
 
-import java.io.IOException;
+import java.io.File;
 import java.util.*;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 
 public class ServerGenerator {
 
     static String MODULE = "business";
-    static String Domain = "Section";
-    static String domain = "section";
+    //static String Domain = "Section";
+    //static String domain = "section";
     static String toServicePath = "server/src/main/java/com/mooc/server/service/";
     static String toControllerPath = MODULE + "/src/main/java/com/mooc/" + MODULE + "/controller/admin/";
     static String toDtoPath = "server/src/main/java/com/mooc/server/dto/";
+    static String generatorConfigPath = "server/src/main/resources/generator/generatorConfig.xml";
+
 
 
     public static void main(String[] args) throws Exception {
-        List<Field> fieldList = DbUtil.getColumnByTableName(domain);
+
+        // only generate first element in table
+        File file = new File(generatorConfigPath);
+        SAXReader reader=new SAXReader();
+        //read xml to document
+        Document doc=reader.read(file);
+        //get the root element of xml
+        Element rootElement=doc.getRootElement();
+        //read context element
+        Element contextElement = rootElement.element("context");
+        //declare an element
+        Element tableElement;
+        //get first table element
+        tableElement=contextElement.elementIterator("table").next();
+        String Domain = tableElement.attributeValue("domainObjectName");
+        String tableName = tableElement.attributeValue("tableName");
+        //String tableNameCn = DbUtil.getTableComment(tableName);
+        String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
+        System.out.println("Table："+tableElement.attributeValue("tableName"));
+        System.out.println("Domain："+tableElement.attributeValue("domainObjectName"));
+        
+        
+        List<Field> fieldList = DbUtil.getColumnByTableName(tableName);
         Set<String> typeSet = getJavaTypes(fieldList);
         Map<String, Object> map = new HashMap<>();
 
+        
 
         map.put("Domain", Domain);
         map.put("domain", domain);
